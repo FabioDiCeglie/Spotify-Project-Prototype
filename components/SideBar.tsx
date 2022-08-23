b42a32688ecc70c904991be9f51ee046179904ec
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-23 22:28:48 +0200

    start working on the sidebar

---
+++ components/SideBar.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const SideBar: NextPage = () => {
+  return (
+    <div>
+      <h1> I'm sidebar</h1>
+    </div>
+  );
+};
+
+export default SideBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import SideBar from "../components/SideBar";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Spotify</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="">
+      <main>
+        <SideBar />
+        {/* Center */}
+      </main>
+
+      {/* <div> Player</div> */}
     </div>
   );
 };
