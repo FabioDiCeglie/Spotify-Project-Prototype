03b1dd6fdd88e0fe1ce8ad468f4ae3a65467a70c
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-23 22:39:28 +0200

    creating the first part of the sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from "next";
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/outline";
 
 const SideBar: NextPage = () => {
   return (
-    <div>
-      <h1> I'm sidebar</h1>
+    <div className="text-gray-500">
+      <div>
+        <button className="flex items-center space-x-2 hover:text-white ">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+      </div>
     </div>
   );
 };
