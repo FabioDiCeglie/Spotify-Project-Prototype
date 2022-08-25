931a5c5b343ac676d160446c63716c0af2ff1f38
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-25 13:02:58 +0200

    fix: fixed sideBar spacing

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -11,7 +11,7 @@ import {
 const SideBar: NextPage = () => {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
-      <div>
+      <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white ">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -25,8 +25,7 @@ const SideBar: NextPage = () => {
           <p>Your Library</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
-      </div>
-      <div>
+
         <button className="flex items-center space-x-2 hover:text-white ">
           <PlusCircleIcon className="h-5 w-5" />
           <p>Create Playlist</p>
