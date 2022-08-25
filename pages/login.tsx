6aff6975a6494c06ee0776236db523bdf9b64834
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-25 14:31:08 +0200

    chore: start working on the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,31 @@
 import type { NextPage } from "next";
+import { getProviders, signIn } from "next-auth/react";
 
-const Login: NextPage = () => {
+const Login: NextPage = ({ providers }: any) => {
   return (
-    <div className="">
-      <h1>This is login</h1>
+    <div>
+      <img
+        className="w-52 mb-5"
+        src="https://links.papareact.com/9xl"
+        alt="Spotify logo"
+      />
+      {Object.values(providers).map((provider: any) => (
+        <div key={provider.name}>
+          <button className="bg-[#18D860]">Login with {provider.name}</button>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
