e8c808d1032c3ebf5abdfdbbe31833eb214e9912
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-25 15:02:28 +0200

    chore: start working on the logout

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -11,6 +11,7 @@ import { signOut, useSession } from "next-auth/react";
 
 const SideBar: NextPage = () => {
   const { data: session, status } = useSession();
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
