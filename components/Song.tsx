805eac982d88f33fb6b61616ca1318823a2f28b5
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-30 19:56:03 +0200

    chore: set up song atom

--- components/Song.tsx
+++ components/Song.tsx
@@ -1,13 +1,30 @@
 import type { NextComponentType } from "next";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
 import { convertMsToMinutesSeconds } from "../lib/time";
 
 const Song: NextComponentType = ({ track, order }: any) => {
   const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+    setCurrentTrackId(trackInfo.id);
+    setIsPlaying(true);
+    spotifyApi.play({
+      uris: [trackInfo.uri],
+    });
+  };
 
   const { track: trackInfo } = track;
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div
+      className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer"
+      onClick={playSong}
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img className="h-10 w-10" src={trackInfo.album.images[0].url} alt="" />
