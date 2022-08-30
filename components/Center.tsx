5713bbe3e684f1e6a677eb9a92bbd1cad5818feb
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-30 19:47:58 +0200

    chore: create song component

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,6 +1,6 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import type { NextPage } from "next";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
@@ -36,9 +36,12 @@ const Center: NextPage = () => {
   }, [spotifyApi, playlistId]);
 
   return (
-    <div className="flex-grow text-white h-screen overflow-y-scroll scrollbar-hide">
+    <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+        <div
+          className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2  text-white  "
+          onClick={() => signOut()}
+        >
           <img
             className="rounded-full w-10 h-10"
             // @ts-ignore
--- components/Song.tsx
+++ components/Song.tsx
@@ -1,24 +1,25 @@
 import type { NextComponentType } from "next";
 import useSpotify from "../hooks/useSpotify";
+import { convertMsToMinutesSeconds } from "../lib/time";
 
 const Song: NextComponentType = ({ track, order }: any) => {
   const spotifyApi = useSpotify();
 
   const { track: trackInfo } = track;
   return (
-    <div className=" grid grid-cols-2">
+    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img className="h-10 w-10" src={trackInfo.album.images[0].url} alt="" />
         <div>
-          <p>{trackInfo.name}</p>
-          <p>{trackInfo.artists[0].name}</p>
+          <p className="w-36 lg:w-64 text-white truncate">{trackInfo.name}</p>
+          <p className="w-40 ">{trackInfo.artists[0].name}</p>
         </div>
       </div>
 
       <div className="flex items-center justify-between ml-auto md:ml-0">
-        <p className="hidden md:inline">{trackInfo.album.name}</p>
-        <p>duration</p>
+        <p className="w-40 hidden md:inline">{trackInfo.album.name}</p>
+        <p>{convertMsToMinutesSeconds(trackInfo.duration_ms)}</p>
       </div>
     </div>
   );
