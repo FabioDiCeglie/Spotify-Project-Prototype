3bd7655c2f7b55310b01dc643dbc1b619d370710
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-30 19:39:01 +0200

    chore: added time function

--- components/Song.tsx
+++ components/Song.tsx
@@ -6,7 +6,7 @@ const Song: NextComponentType = ({ track, order }: any) => {
 
   const { track: trackInfo } = track;
   return (
-    <div className="text-white">
+    <div className=" grid grid-cols-2">
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img className="h-10 w-10" src={trackInfo.album.images[0].url} alt="" />
@@ -14,11 +14,11 @@ const Song: NextComponentType = ({ track, order }: any) => {
           <p>{trackInfo.name}</p>
           <p>{trackInfo.artists[0].name}</p>
         </div>
+      </div>
 
-        <div>
-          <p>{trackInfo.name}</p>
-          <p>{trackInfo.artists[0].name}</p>
-        </div>
+      <div className="flex items-center justify-between ml-auto md:ml-0">
+        <p className="hidden md:inline">{trackInfo.album.name}</p>
+        <p>duration</p>
       </div>
     </div>
   );
