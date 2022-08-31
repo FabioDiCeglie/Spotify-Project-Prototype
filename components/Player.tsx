840959648e23b87cf15e0dcfc06950b38606c17c
FabioDiCeglie <dcfabio96@gmail.com>
2022-08-31 20:24:45 +0200

    chore: working on fetch song when we refresh page

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,8 +1,9 @@
 import type { NextComponentType } from "next";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSongInfo from "../hooks/useSongInfo";
 import useSpotify from "../hooks/useSpotify";
 
 const Player: NextComponentType = () => {
@@ -12,10 +13,37 @@ const Player: NextComponentType = () => {
     useRecoilState(currentTrackIdState);
   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
   const [volume, setVolume] = useState(50);
+
+  const songInfo = useSongInfo();
+
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then((data: any) => {
+        setCurrentTrackId(data.body?.item.id);
+
+        spotifyApi.getMyCurrentPlayblackState().then((data: any) => {
+          setIsPlaying(data.body.is_playing);
+        });
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      fetchCurrentSong();
+      setVolume(50);
+    }
+  }, [currentTrackId, spotifyApi, session]);
+
   return (
     <div className="">
       <div>
-        <img src="" alt="" />
+        <img
+          className="hidden md:inline h-10 w-10"
+          // @ts-ignore
+          src={songInfo?.album.images?.[0]?.url}
+          alt=""
+        />
       </div>
 
       <div></div>
