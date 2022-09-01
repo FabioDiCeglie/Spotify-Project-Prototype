a2b61169f75438812664eb4f1ffe05a7f786c65a
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-01 19:32:10 +0200

    chore: create useEffect for volume

--- components/Player.tsx
+++ components/Player.tsx
@@ -60,6 +60,11 @@ const Player: NextComponentType = () => {
     }
   }, [currentTrackId, spotifyApi, session]);
 
+  useEffect(() => {
+    if (volume > 0 && volume < 100) {
+      debouncedAdjustVolume(volume);
+    }
+  }, [volume]);
   return (
     <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
       <div className="flex items-center space-x-4">
@@ -95,16 +100,22 @@ const Player: NextComponentType = () => {
       </div>
 
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-        <VolumeDownIcon className="button" />
+        <VolumeDownIcon
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+          className="button"
+        />
         <input
           className="w-14 md:w-28"
           type="range"
           min={0}
           max={100}
           value={volume}
-          //   onChange={""}
+          onChange={(e) => setVolume(Number(e.target.value))}
         />
-        <VolumeUpIcon className="button" />
+        <VolumeUpIcon
+          onClick={() => volume < 100 && setVolume(volume + 10)}
+          className="button"
+        />
       </div>
     </div>
   );
