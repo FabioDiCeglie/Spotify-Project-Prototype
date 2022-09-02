0b8c1c8896509f9b8fcea0f0c8266c05ab5bc043
FabioDiCeglie <dcfabio96@gmail.com>
2022-09-02 15:19:34 +0200

    bump vers

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,18 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/")) {
-    // Token will exist if user is logged in
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+// export async function middleware(req: NextRequest) {
+//   if (req.nextUrl.pathname.startsWith("/")) {
+//     // Token will exist if user is logged in
+//     const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    const { pathname } = req.nextUrl;
+//     const { pathname } = req.nextUrl;
 
-    // Allow the requests if the following is true...
-    if (pathname.includes("/api/auth") || token) {
-      return NextResponse.next();
-    }
+//     // Allow the requests if the following is true...
+//     if (pathname.includes("/api/auth") || token) {
+//       return NextResponse.next();
+//     }
 
     // // Redirect them to login if they don't have a token
     // if (!token && pathname !== "/login") {
